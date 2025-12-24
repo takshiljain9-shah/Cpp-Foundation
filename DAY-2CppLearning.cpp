1f4e92976cb1e564f53a2d44118e72b61ddaa071
takshiljain9-shah <Takshiljain.shah@gmail.com>
2025-12-24 20:34:38 +0530

    Day 2

---
+++ DAY-2CppLearning.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    int age;
+
+    cout << "--- New Beginnings ---" << endl;
+    cout << "Enter Your Name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << endl;
+
+    cout << "Welcome, " << name << "!" << endl;
+    cout << "At " << age << "years young, your potential is limitless." << endl;
+    cout << "Today is a fresh opportunity to build something amazing." << endl;
+    cout << "believe in yourself and keep coding!" << endl;
+
+    return 0;
+}
